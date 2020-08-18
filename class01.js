8524cad839c0ca0684a984bacbacc22d8c36a7bd
amanuelgezahange12 <69604107+amanuelgezahange12@users.noreply.github.com>
2020-08-17 20:33:17 -0500

    First Assignment complete

---
+++ class01.js
@@ -0,0 +1,45 @@
+//Assignment 1
+let answer=`Question 1
+input-area of a lawn
+process-multiply by 0.01,multiply by 15
+         
+output-season cost
+
+Question 2
+step 1-get the total area of lawn from user
+step 2-multiply the area of the lawn by 0.01 & then by 15 to get the season cost
+step 3-Display the season cost for the user to see
+
+Question 3
+input-number of regular hours,overtime hours,regular hourly wage
+process-all the rest activities
+output-net pay
+
+Question 4
+step 1-get regular hours,overtime hours,regular hourly wage
+step 2-regpay=regHour*regWage
+step3-overpay=overhour*regWage*1.5
+step4-grosspay=regpay+overpay
+step5-tax=grossPay*0.15
+step6-netPay=grossPay-tax
+step7-display net pay
+
+Question 5
+selection
+sequence
+repetition
+try,catch and throw
+
+Question 5
+select
+repetition 
+sequence`
+console.log(answer)
+
+
+
+
+
+
+
+
